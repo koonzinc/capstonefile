7aedb2841d0170a4e40f5711a2e4aaf1ae123064
koonzinc <koonz.eth@gmail.com>
2022-10-19 18:33:44 -0400

    import components

---
+++ src/components/ScrollingStocks/ScrollingStocks.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import "./ScrollingStocks.css";
+
+function ScrollingStocks() {
+  return (
+    <div className="scroll__parent">
+      <div className="scroll__element primary"></div>
+      <div className="scroll__element secondary"></div>
+    </div>
+  );
+}
+
+export default ScrollingStocks;
