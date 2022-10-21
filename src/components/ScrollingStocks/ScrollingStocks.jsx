b6ea0b70c99e7f314de1b0688849efdaeafb67be
koonzinc <koonz.eth@gmail.com>
2022-10-21 17:48:42 -0400

    fix

--- src/components/ScrollingStocks/ScrollingStocks.jsx
+++ src/components/ScrollingStocks/ScrollingStocks.jsx
@@ -1,12 +1,42 @@
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./ScrollingStocks.css";
+import axios from 'axios';
+import {Link} from 'react-router-dom';
 
 function ScrollingStocks() {
+  const [symbol, setSymbol] = useState('')
+
+  const url = 'https://finnhub.io/api/v1/search?q=AAPL&token=cd9g9uaad3i97v8iuvfgcd9g9uaad3i97v8iuvg0'
+
+  useEffect(() => {
+    getSymbol();
+  }, [])
+
+  const getSymbol = async () => {
+    const response = await axios.get(url);
+    const theSymbol = response.data.result[0].symbol;
+    setSymbol(theSymbol);
+
+  }
+
   return (
     <div className="scroll__parent">
-      <div className="scroll__element primary"></div>
-      <div className="scroll__element secondary"></div>
+      <div style={{ color: 'white' }} className="scroll__element primary">
+        <Link to='/individual-stocks'><a className='tablet symbol'>{symbol}</a></Link>
+        <Link to='/individual-stocks'><a className='tablet symbol'>{symbol}</a></Link>
+        <Link to='/individual-stocks'><a className='tablet symbol'>{symbol}</a></Link>
+        <Link to='/individual-stocks'><a className='tablet symbol'>{symbol}</a></Link>
+        <Link to='/individual-stocks'><a className='tablet symbol'>{symbol}</a></Link>
+        
+      </div>
+      <div style={{ color: 'white' }} className="scroll__element secondary">
+      <Link to='/individual-stocks'><a className='tablet symbol'>{symbol}</a></Link>
+      <Link to='/individual-stocks'><a className='tablet symbol'>{symbol}</a></Link>
+      <Link to='/individual-stocks'><a className='tablet symbol'>{symbol}</a></Link>
+      <Link to='/individual-stocks'><a className='tablet symbol'>{symbol}</a></Link>
+      <Link to='/individual-stocks'><a className='tablet symbol'>{symbol}</a></Link>
+      </div>
     </div>
   );
 }
