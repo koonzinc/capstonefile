66369bf497e44a6295e6a322097f8c1649fdc311
Isaiah Desta <isaiahdesta@Isaiahs-MacBook-Pro.local>
2022-10-21 15:43:27 -0400

    ernesto saves the day.

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,28 +1,35 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-
 const Api = () => {
-    const [price, setPrice] = useState('');
-    const URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=TSLA&apikey=FLJHIE0AGGIL5426";
-    
-    useEffect(() => {
-        getPrice();
-    }, []);
-    
-    const getPrice = () => {
-        axios.get(URL)
-        .then((response) => {
-            const thePrice = response.data.close;
-            setPrice(thePrice);
-        })
-        .catch(error => console.error( `Error: ${error}`));
+  const [price, setPrice] = useState("");
+  const [close, setClose] = useState([]);
+  const URL =
+    "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=XCI1NEPICS3X7H9B";
 
-        return (
-            <div>
-              <h1>{price}</h1>
-            </div>
-          );
-    }}
+  useEffect(() => {
+    close.length <= 0 && getPrice();
 
-    export default Api;
+    close.length > 0 && setPrice(close[0][1]["4. close"]);
+  }, [close]);
+
+  useEffect(() => {
+    console.log(price);
+  }, [price]);
+
+  const getPrice = async () => {
+    const response = await axios.get(URL);
+    const thePrice = response.data["Time Series (Daily)"];
+    thePrice && setClose(Object.entries(thePrice));
+
+    console.log(response.data);
+  };
+
+  return (
+    <div>
+      <h1>{price && price}</h1>
+    </div>
+  );
+};
+
+export default Api;
--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Signup from './pages/Signup/Signup';
 import IndividualStocks from './pages/IndividualStocks/IndividualStocks';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
 import Home from './pages/Home/Home';
+import Api from './API/Api';
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}/>
+      <Route path="/" element={<Api />}/>
       <Route path="/individual-stocks" element={<IndividualStocks />}/>
       <Route path="/profile-page" element={<ProfilePage />}/>
       <Route path="/signup" element={<Signup />}/>
