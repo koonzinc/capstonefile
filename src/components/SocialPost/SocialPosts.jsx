3f86906224e73576af6c2ac189dde7c1e81253c7
Isaiah Desta <isaiahdesta@Isaiahs-MBP.attlocal.net>
2022-10-30 17:51:17 -0400

    stable v

--- src/components/SocialPost/SocialPosts.jsx
+++ src/components/SocialPost/SocialPosts.jsx
@@ -4,18 +4,20 @@ import { AiOutlineCloseCircle } from "react-icons/ai";
 import axios from "../../axios";
 
 export default function SocialPosts({ modal, handleModal }) {
-  const [headlineData, setHeadlineData] = useState({});
-  const [content, setContent] = useState({});
+  const [headlineData, setHeadlineData] = useState("");
+  const [content, setContent] = useState("fhjfbhdsujbfsjhd");
   const [date, setDate] = useState("01-01-01");
+  const [user, setUser] = useState(1);
 
   const createPost = (e) => {
     e.preventDefault();
     axios
       .post("posts", {
+        user_id: user,
         headline: headlineData,
         content: content,
         date_stamp: date,
-      })
+      },)
 
       .then((res) => {
         console.log("API response: ", res);
@@ -48,21 +50,24 @@ export default function SocialPosts({ modal, handleModal }) {
               alt=""
             />
             <div className="content__wrapper">
-              <input
-                onChange={(e) => setHeadlineData(e.target.value)}
-                className="modal__headline"
-                placeholder="headline"
-              />
-              <textarea
+              <form onSubmit={createPost}>
+                <input
+                  onChange={(e) => setHeadlineData(e.target.value)}
+                  className="modal__headline"
+                  placeholder="headline"
+                />
+
+                {/* <textarea
                 onChange={(e) => setContent(e.target.value)}
                 className="textarea"
                 placeholder="what's happening?"
                 rows="7"
-              ></textarea>
+              ></textarea> */}
+              </form>
             </div>
           </div>
           <div className="button__container">
-            <button onClick={createPost} type="submit" className="send">
+            <button type="submit" className="send">
               Send
             </button>
           </div>
