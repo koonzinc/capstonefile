574c433c847505f7d00ebbeb78bf39556ec0272a
Isaiah Desta <isaiahdesta@Isaiahs-MacBook-Pro.local>
2022-11-01 15:52:20 -0400

    goodstocks css fixed

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL:
-    "https://3000-areandd-capstonebackend-bqezw6kimlg.ws-us73.gitpod.io/",
+    "https://3000-areandd-capstonebackend-0e1ygi930oc.ws-us73.gitpod.io/",
 });
 
 export default instance;
