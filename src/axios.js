2bd092013fd490428e0037d46caff9c56146a62d
koonzinc <koonz.eth@gmail.com>
2022-11-02 15:34:09 -0400

    update

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://3000-areandd-capstonebackend-u0k2n3qbts9.ws-us73.gitpod.io/",
+  baseURL: "http://192.168.1.68:3000/admin/",
 });
 
 export default instance;
